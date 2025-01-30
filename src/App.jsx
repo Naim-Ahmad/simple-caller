75e318cdf9f16379ee385f05469b4f949b9915f0
Naim Ahmad <naimahmad201103@gmail.com>
2025-01-30 07:59:32 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Caller from './Caller';
+
+function App() {
+  return (
+    <div className="App">
+      <Caller />
+    </div>
+  );
+}
+
+export default App;
